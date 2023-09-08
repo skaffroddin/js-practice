3e1cd31553ddf3723f63487bbb4497a04f7adba0
skaffroddin <skaffroddin4@gmail.com>
2023-09-08 15:04:58 +0530

    code added

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-//add two numbwers
-const num1 = parseInt (prompt('enter first number:'));
-const num2 = parseInt (prompt('Enter secound number'));
-const sum = num1 + num2;
-console.log(`The sum of ${num1} and ${num2} is ${sum}`)
+// //add two numbwers
+// const num1 = parseInt (prompt('enter first number:'));
+// const num2 = parseInt (prompt('Enter secound number'));
+// const sum = num1 + num2;
+// console.log(`The sum of ${num1} and ${num2} is ${sum}`)
+
+
+// variable naming 
+// var _myName ="Affroddin";
+// var _1my__Name = "Affroddin";
+// var $myname = "Affroddin";      
+
+// console.log($myname);
