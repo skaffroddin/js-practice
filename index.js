0e1b648326b27f5c6d574d364d6d7895c16377ff
skaffroddin <skaffroddin4@gmail.com>
2023-09-08 14:43:22 +0530

    code added

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("hello")
+//add two numbwers
+const num1 = parseInt (prompt('enter first number:'));
+const num2 = parseInt (prompt('Enter secound number'));
+const sum = num1 + num2;
+console.log(`The sum of ${num1} and ${num2} is ${sum}`)
